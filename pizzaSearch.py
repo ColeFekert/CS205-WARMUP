4b2efbe83f5aed5318453d33b419846eb1e246bd
Nick <38964668+nebouff@users.noreply.github.com>
2019-09-09 15:12:57 -0400

    Python file now has content

---
+++ pizzaSearch.py
@@ -0,0 +1,28 @@
+# Nick Bouffard, Cole Fekert, Kyle Mac, and Henry Rice
+# CS 205 - Warmup Project
+# Pizza Search tool
+
+def main():
+
+    continue = 1
+
+    # Introduction to program
+    print("Welcome to PizzaSearch3000")
+    print("Please enter search query")
+
+    # Call database integration function
+    createTables()
+
+    while continue == 1
+        # Call search function
+        # Ask if user would like to search again
+        # IF no, continue = 0
+
+def createTables():
+    # Load CSVs into database tables
+
+def search(query):
+    # Receive sarch query and interact with database appropriately
+
+
+main()
