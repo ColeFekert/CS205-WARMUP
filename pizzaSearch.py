4f051e161eb138a6197a2c68faacf82da7255e70
mkyle98 <kylem.sde@gmail.com>
2019-09-10 17:05:40 -0400

    Added pizza.csv to database, needs troubleshooting on the syntax errors

--- pizzaSearch.py
+++ pizzaSearch.py
@@ -2,9 +2,11 @@
 # CS 205 - Warmup Project
 # Pizza Search tool
 
+import sqlite3, csv
+
 def main():
 
-    continue = 1
+    #continue = 1
 
     # Introduction to program
     print("Welcome to PizzaSearch3000")
@@ -13,12 +15,27 @@ def main():
     # Call database integration function
     createTables()
 
-    while continue == 1
+    #while continue == 1
         # Call search function
         # Ask if user would like to search again
         # IF no, continue = 0
 
 def createTables():
+        #Create database
+        conn = sqlite3.connect('pizzaCities.db')
+        c = conn.cursor()
+        
+        #Create table 1 - Pizzas
+        c.execute("CREATE TABLE pizzas (address text, categories text, city text, keys text, menusAmountMax real, menusAmountMin real, name text, postalCode integer, priceRangeMin integer, priceRangeMax integer, province text) ")
+        with open('pizza.csv', 'r') as pizzaTable:
+                data = csv.DictReader(pizzaTable)
+                toDB = [(i['address'], i['categories'], i['city'], i['keys'], i['menus.AmountMax'], i['menus.AmountMin'], i['name'], i['postalCode'], i['priceRangeMin'], i['priceRangeMax'], i['province']) for i in data]
+        c.executemany("INSERT INTO pizzaCities VALUES (?,?,?,?,?,?,?,?,?,?,?);", toDB)
+        conn.commit()
+
+
+
+        
     # Load CSVs into database tables
 
 def search(query):
