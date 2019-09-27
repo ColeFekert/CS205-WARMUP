c35e0afd774dbcf32d3216db94868a2975bf9aea
ColeFekert <cfekert@gmail.com>
2019-09-27 16:10:33 -0400

    - Added a bunch of functions

--- pizzaSearch.py
+++ pizzaSearch.py
@@ -63,19 +63,198 @@ def createTables():
 # conn = sqlite3.connect('pizzaCities.db')
 #         c = conn.cursor()
         
+def verifyQuery(query):
+
+    possibleQueryElements = ["cities", "pizza places", "postal code"]
+    ignorableQueryElements = ["with", "in", "for"]
+    calculationQueryElements = ["price", "population", "cardinal direction"]
+
+
+
+    for i in range(len(query)):
+
+
+
+
+def getSectionalQuery():
+    sectionalQuery = ""
+    inputNotApproved = True
+
+    while inputNotApproved:
+        sectionalQuery = input("Enter sectional query: ")
+
+        sectionalQuery.lower()
+
+        if "pizza places" in sectionalQuery:
+            return "pizza places "
+        elif "cities" in sectionalQuery:
+            return "cities "
+        elif "postal code" in sectionalQuery:
+            return "postal code "
+        else:
+            inputNotApproved = True
+
+
+def getRestrictiveQuery():
+    inputNotApproved = True
+    restrictiveQuery = []
+
+    while inputNotApproved:
+        restrictiveQuery[0] = input("Enter the category you want to narrow down: ")
+
+        secondInputNotApproved = True
+        restrictiveQuery[0].lower()
+
+        if "price" in restrictiveQuery[0]:
+            restrictiveQuery[0] = "price "
+            while secondInputNotApproved:
+                restrictiveQuery[1] = input("Enter price ($,$$,$$$):  ")
+
+                if restrictiveQuery[1].isalpha():
+                    secondInputNotApproved = True
+                else:
+                    secondInputNotApproved = False
+            return restrictiveQuery
+
+        elif "population" in restrictiveQuery[0]:
+            while secondInputNotApproved:
+                restrictiveQuery[1] = input("Enter max population: ")
+
+                if restrictiveQuery[1].isalpha():
+                    secondInputNotApproved = True
+                else:
+                    secondInputNotApproved = False
+            return restrictiveQuery
+        elif "cardinal location" in restrictiveQuery[0]:
+            while secondInputNotApproved:
+                restrictiveQuery[1] = input("Enter cardinal direction: ")
+
+                restrictiveQuery[1].lower()
+
+                if restrictiveQuery[1].isdecimal() or restrictiveQuery[1].isdigit():
+                    secondInputNotApproved = True
+                elif "north" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "south" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "east" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "west" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "northeast" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "northwest" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "southeast" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                elif "southwest" in restrictiveQuery[1]:
+                    secondInputNotApproved = False
+                else:
+                    secondInputNotApproved = True
+
+            return restrictiveQuery
+        else:
+            inputNotApproved = True
+
+
+def getModifierQuery():
+    inputNotApproved = True
+
+    while inputNotApproved:
+        modifierQuery = input("Enter modifier to previous Query: ")
+
+        modifierQuery.lower()
+
+        if "with" in modifierQuery:
+            return "with "
+        elif "in" in modifierQuery:
+            return "in "
+        else:
+            inputNotApproved = True
+
 
 def search():
-    # Receive sarch query and interact with database appropriately
+    # Receive search query and interact with database appropriately
     c = sqlite3.connect('pizzaCities.db')
     cur = c.cursor()
-    x = input("testing: ")
-    if x.startswith("population"):
-            value = x.partition('population ')[2]
-            cur.execute("SELECT population FROM cities WHERE city =?", (value,))
-            try:
-                    fetch = cur.fetchone()[0]
-                    print(fetch)
-            except:
-                    print("An exception has occured")
+
+    # %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%& GET INPUT FROM USER %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+
+    continueQuery = True
+    queryApproved = False
+
+    query = []
+
+    while not queryApproved:
+        query = input("Enter the query: ").split()
+
+        queryApproved = verifyQuery(query)
+
+
+
+    # query = [getSectionalQuery(), getModifierQuery()]
+
+    if query[1] == "with ":
+        query.append(getRestrictiveQuery())
+    else:
+        query.append(getSectionalQuery())
+
+    cont = input("Add more to the query? (y/n) ")
+    if cont == 'y':
+        continueQuery = True
+    else:
+        continueQuery = False
+
+    # possibly limit how many times we allow a user to extend a query?
+    while continueQuery:
+        if query[len(query)] == "with ":
+            query.append(getRestrictiveQuery())
+            query.append(getModifierQuery())
+        else:
+            query.append(getSectionalQuery())
+            query.append(getModifierQuery())
+
+    # %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%& BUILD QUERY %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+    # Need to add handling for 'number of'
+    executionString = "SELECT "
+    for i in range(len(query)):
+        if (i % 2 == 0):
+            if query[i - 1] == "with ":
+                # this means we are on a restrictive query
+                executionString += query[i][0]
+                executionString += "< "
+                executionString += query[i][1]
+
+            # this means we are on a sectional query
+            executionString += query[i]
+
+        else:
+            # this means we are on a modifier query
+            if query[i] == "with ":
+                executionString += "WHERE "
+            else:
+                executionString += "FROM "
+
+    cur.execute(executionString)
+    # %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%& QUERY THE DATABASE %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+    # %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%& Older code %&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+
+    print("Enter first part of query below:")
+    print("Examples: 'pizza places', 'cities', or 'number of'")
+    q1 = input("First part of query: ")
+
+    if q1.startswith("population"):
+        value = q1.partition('population ')[2]
+        cur.execute("SELECT population FROM cities WHERE city =?", (value,))
+        try:
+            fetch = cur.fetchone()[0]
+            print(fetch)
+        except:
+            print("An exception has occurred")
+
+    elif q1.startswith("number"):
+        value = q1.partition('number ')
+        # need the rest of query before executing
+
 
 main()
